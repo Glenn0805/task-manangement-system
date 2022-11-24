129420c67a1ff0335be97fcbbb27c6b571ce8278
Glen0805 <cavitaglenn22131@gmail.com>
2022-11-24 12:21:38 +0800

    create tables

---
+++ database/migrations/1669263384394_tbl_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tbl_users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('user_name').notNullable()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.string('password').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamps()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
